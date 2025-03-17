e057fed461661c6e04124e56b53d821e9e90d51e
mmyeon <mindfulyeon@gmail.com>
2025-03-17 16:23:59 +0900

    [feat] Radix ui 설치 및 theme 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme accentColor="grass" grayColor="sand" radius="large">
+          {children}
+        </Theme>
       </body>
     </html>
   );
