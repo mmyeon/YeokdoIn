5110c1e22bcee3a703b273a35c1d4e2d0aba2395
mmyeon <mindfulyeon@gmail.com>
2025-03-20 20:15:14 +0900

    [fix] window 객체 하이드레이션 에러 개선

--- app/hooks/useLocalStorage.tsx
+++ app/hooks/useLocalStorage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export type StorageKey =
   | "cleanRecord"
   | "snatchRecord"
@@ -6,15 +8,24 @@ export type StorageKey =
   | "programWeights";
 
 export function useLocalStorage() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   function getLocalStorageItem(key: StorageKey): string {
+    if (!isClient) return "";
     return window.localStorage.getItem(key) || "";
   }
 
   function setLocalStorageItem(key: StorageKey, value: string): void {
+    if (!isClient) return;
     window.localStorage.setItem(key, value);
   }
 
   function removeLocalStorageItem(key: StorageKey): void {
+    if (!isClient) return;
     window.localStorage.removeItem(key);
   }
 
