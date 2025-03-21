41646adea7895013036ff630b48098ddeea0dbf6
mmyeon <mindfulyeon@gmail.com>
2025-03-21 20:37:17 +0900

    [feat] 메인 페이지 문구 및 간격 수정

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const { setLocalStorageItem } = useLocalStorage();
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="w-screen h-screen flex items-center justify-center p-5">
       <div className="flex flex-col items-center justify-center w-screen h-screen gap-4 max-w-md">
         <Image
           src="/logo.svg"
@@ -24,7 +24,10 @@ export default function Home() {
           priority
         />
 
-        <h1 className="text-2xl font-bold">운동 종목을 선택해 주세요.</h1>
+        <h1 className="text-2xl font-bold">
+          오늘 어떤 종목을 훈련하실 건가요?
+        </h1>
+        <span>훈련할 종목을 선택해 주세요!</span>
 
         <div className="flex flex-col gap-4 w-full flex-">
           {BUTTONS.map(({ value, label }) => (
