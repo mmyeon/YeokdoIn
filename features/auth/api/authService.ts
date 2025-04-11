c914ffea2a2901ade5758bdc46841ff1328ac676
mmyeon <mindfulyeon@gmail.com>
2025-04-11 11:10:33 +0900

    [feat] Supabase 오류 처리 개선 및 사용자 친화적인 오류 메시지 추가

--- features/auth/api/authService.ts
+++ features/auth/api/authService.ts
@@ -1,10 +1,37 @@
 import { supabaseClient } from "@/shared/api/supabaseClient";
-import { AuthError } from "@supabase/supabase-js";
+import { AuthError, isAuthApiError } from "@supabase/supabase-js";
+
+class AuthServiceError extends Error {
+  constructor(
+    public message: string,
+    public status?: number,
+  ) {
+    super(message);
+    this.name = "AuthServiceError";
+  }
+}
 
 const handleSupabaseError = (error: AuthError | null) => {
-  if (error) {
-    console.error("Supabase error:", error.message);
-    throw new Error(error.message);
+  if (isAuthApiError(error)) {
+    let userFriendlyMessage = "알 수 없는 오류가 발생했습니다.";
+
+    switch (error.message) {
+      case "Invalid login credentials":
+        userFriendlyMessage = "이메일 또는 비밀번호가 잘못되었습니다.";
+        break;
+      case "Email not confirmed":
+        userFriendlyMessage = "이메일 인증이 필요합니다.";
+        break;
+      case "User not found":
+        userFriendlyMessage = "가입되지 않은 이메일입니다.";
+        break;
+      case "Password should be at least 8 characters":
+        userFriendlyMessage = "비밀번호는 8자 이상이어야 합니다.";
+        break;
+      default:
+        console.error("Unhandled Supabase error:", error.message);
+    }
+    throw new AuthServiceError(userFriendlyMessage, error.status);
   }
 };
 
