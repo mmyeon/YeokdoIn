e0e860679a24b606cd444e8721a75a54cfde828f
mmyeon <mindfulyeon@gmail.com>
2025-04-17 13:03:38 +0900

    [refactor] 회원가입 관련 폼 및 유효성 검사 코드 삭제

--- shared/form/schemas.ts
+++ shared/form/schemas.ts
@@ -11,17 +11,3 @@ export const barWeightSchema = z
     invalid_type_error: FORM_ERROR_MESSAGES.invalidNumberType,
   })
   .min(1, "바벨 무게는 1kg 이상이어야 합니다.");
-
-export const emailSchema = z
-  .string()
-  .email("올바른 이메일 형식을 입력해주세요.");
-
-export const passwordSchema = z
-  .string()
-  .regex(/[A-Za-z]/, "최소 하나의 문자가 포함되어야 합니다.")
-  .regex(/\d/, "최소 하나의 숫자가 포함되어야 합니다.")
-  .regex(
-    /[@$!%*?&]/,
-    "최소 하나의 특수문자(@, $, !, %, *, ?, &)가 포함되어야 합니다.",
-  )
-  .min(8, "비밀번호는 최소 8자 이상이어야 합니다.");
