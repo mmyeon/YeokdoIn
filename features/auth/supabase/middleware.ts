b3e20fec12b1d45b76fccb600eeb1de777d390f4
mmyeon <mindfulyeon@gmail.com>
2025-06-25 15:44:30 +0900

    Revert "[chore] 인증 오류 개선하기 위해서 미들웨어 경로와 쿠키 정보 로그하는 로직 추가"
    This reverts commit 797e0af6c6c2c5b5e28984eda23bc7ff016bab5a.

--- features/auth/supabase/middleware.ts
+++ features/auth/supabase/middleware.ts
@@ -35,13 +35,8 @@ export async function updateSession(request: NextRequest) {
   // issues with users being randomly logged out.
 
   const {
-    data: { user }, error
+    data: { user },
   } = await supabase.auth.getUser();
-
-  console.log("@@ middleware user", user)   
-
-  if(error) 
-    console.error("Auth error:", error)
 
   // 인증되지 않은 사용자는 로그인 페이지로 리다이렉트
   if (!user) {
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "./features/auth/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  console.log('@@ Middleware path:', request.nextUrl.pathname);
-  console.log('@@ Middleware cookies:', request.cookies.getAll());
   return await updateSession(request);
 }
 
